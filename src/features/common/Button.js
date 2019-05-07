6b8fc0e691d73b7d09e2f826f7aed95f0c5569e5
Barry Melton <barry.melton@gmail.com>
2019-05-07 09:15:23 -0400

    Created a button. Added some states. Added storybook integration.

---
+++ src/features/common/Button.js
@@ -0,0 +1,21 @@
+// src/features/common/Button.js
+
+import React from "react";
+
+import "./Button.css";
+
+const Button = props => (
+  <div
+    className={`Button ${
+      props.active
+        ? props.fill
+          ? `active fill`
+          : `active no-fill`
+        : `disabled`
+    }`}
+  >
+    {props.label}
+  </div>
+);
+
+export default Button;
---
+++ src/stories/index.js
@@ -0,0 +1,16 @@
+// src/stories/index.js
+
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Button from "../../src/features/common/Button";
+
+storiesOf("Button", module)
+    .add("active with fill", () => (
+        <Button label={`continue`} fill={true} active={true} />
+    ))
+    .add("active with no fill", () => (
+        <Button label={`sign up`} fill={false} active={true} />
+    ))
+    .add("disabled", () => (
+        <Button label={`continue`} active={false} />
+    ))
